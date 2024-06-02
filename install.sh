c95cc60d9e5c79dced54414777d2d3f7b7679155
nowcat123 <62401005+nowcat123@users.noreply.github.com>
2024-06-02 19:26:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,16 @@ if [ -z "$BOOTTOTORO" ]
 then
 	echo "You have not set your BOOTTOTORO env variable!"
 	echo "use export BOOTTOTORO=/dev/XXXX"
-else
+else	
+	if [ -z "$USER" ]
+ 	then
+  	echo "You have not set your USERNAME env variable!"
+	echo "use export USER=username"
+ 	else
+  	if [ -z "$PSSWD" ]
+   	then
+    	echo "You have not set your PSSWD env variable!"
+	echo "use export PSSWD=password"
 	echo "Making Filesystems..."
 	mkfs.ext4 $ROOTTOTORO
  	mkfs.fat -F32 $BOOTTOTORO
@@ -18,6 +27,12 @@ else
     	echo "DONE!"
      	echo "Installing Base System..."
     	pacstrap -K /mnt linux linux-firmware base base-devel
+     	echo "MAKING USER"
+ 	arch-chroot /mnt useradd $USER
+  	arch-chroot /mnt passwd $USER < $PSSWD
+   	arch-chroot /mnt mkdir /home/{$USER}/
+	arch-chroot /mnt chown +R {$USER}:{$USER} /home/{$USER}
+   	echo "DONE!"
      	echo "Installing Extra Packages..."
      	wget https://raw.githubusercontent.com/nowcat123/toroto-linux/master/packages.txt -o /mnt/packages.txt
       	arch-chroot /mnt pacman -S - < packages.txt
