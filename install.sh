69388a3682d793514c7965c274d58d3a56da77f4
nowcat123 <62401005+nowcat123@users.noreply.github.com>
2024-06-02 17:15:25 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
-if [ -z "$DISK" ]
+if [ -z "$ROOTTOTORO" ]
 then
-	echo "You have not set your DISK env variable!"
-	echo "use export DISK=/dev/XXX"
+	echo "You have not set your ROOTTOTORO env variable!"
+	echo "use export ROOTTOTORO=/dev/XXXX"
 else
-
+if [ -z "$BOOTTOTORO" ]
+then
+	echo "You have not set your BOOTTOTORO env variable!"
+	echo "use export BOOTTOTORO=/dev/XXXX"
+else
 fdisk $DISK
 fdisk /dev/DEV<<EOF
 n
@@ -22,4 +26,5 @@ EOF
 
 mkfs.fat -F32 {$DISK}1
 mkfs.ext4 {$DISK}2
+fi
 fi
