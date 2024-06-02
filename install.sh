b76c18df866cef44316659f9dd7f32f4c956d152
nowcat123 <62401005+nowcat123@users.noreply.github.com>
2024-06-02 17:55:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,23 @@ then
 	echo "You have not set your BOOTTOTORO env variable!"
 	echo "use export BOOTTOTORO=/dev/XXXX"
 else
+	echo "Making Filesystems..."
 	mkfs.ext4 $ROOTTOTOTORO
  	mkfs.fat -F32 $BOOTTOTORO
+  	echo "DONE!"
+  	echo "Mounting Filesystems..."
   	mount $ROOTTOTOTORO /mnt
    	mount $BOOTTOTOTORO /mnt/boot --mkdir
+    	echo "DONE!"
+     	echo "Installing Base System..."
     	pacstrap -K /mnt linux linux-firmware base base-devel
+     	echo "Installing Extra Packages..."
      	wget https://raw.githubusercontent.com/nowcat123/toroto-linux/master/packages.txt -o /mnt/packages.txt
       	arch-chroot /mnt pacman -S - < packages.txt
-     	bootctl install
+       	echo "DONE!"
+	echo "INSTALLING BOOTLOADER!"
+     	arch-chroot /mnt bootctl install
+      	echo "DONE!"
+	echo "CONFIGURING BOOTLOADER!
 fi
 fi
