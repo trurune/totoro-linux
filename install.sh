9000806bc0f4687ca83ee98621aed42070fd669f
Alex <62401005+trurune@users.noreply.github.com>
2024-07-23 10:49:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ echo "Installing Totoro Linux Osaka 06.2024"
 sleep 2
 clear
 lsblk
-DISK=$(lsblk -d -o NAME | grep -E '^[a-z]' | awk '{print "/dev/" $1}' | fzf --prompt="Select a disk: ")
+echo "What is your disk (/dev/XXX)?"
+read DISK
 if [[ $string =~ "nvme" ]]; then
 export ROOTTOTORO=${DISK}p2
 export BOOTTOTORO=${DISK}p1
@@ -70,12 +71,10 @@ else
     	echo "DONE!"
      	clear
       	echo "INSTALLNG BASE SYSTEM!"
-    	pacstrap -K /mnt linux linux-firmware base base-devel networkmanager
+    	pacstrap -K /mnt linux linux-firmware base base-devel
      	echo "MAKING USER!"
  	arch-chroot /mnt useradd $USER
   	clear
-   	arch-chroot /mnt pacman-key --init
-    	arch-chroot /mnt pacman-key --populate
    	echo "PLEASE SET A PASSWORD FOR THE USER!"
   	arch-chroot /mnt passwd $USER
    	mkdir /mnt/home/$USER
@@ -162,13 +161,16 @@ else
     echo "Please uncomment the line where your locale is (e.g en_US.UTF-8 UTF-8)"
     read DJODJ
     nano /mnt/etc/locale.gen
-    arch-chroot /mnt locale-gen
+    arch-chroot /mny locale-gen
     echo "DONE!"
     clear
-    
-    CONTINENT=$(find /usr/share/zoneinfo -type d | awk -F/ '{print $NF}' | fzf --prompt="Select a zone range")
-    
-	ZONE=$(find /usr/share/zoneinfo/$CONTINENT -type d | awk -F/ '{print $NF}' | fzf --prompt="Select a zone")
+    echo "SETTING TIME ZONE"
+    echo "What is your continent?"
+    ls /usr/share/zoneinfo
+    read CONTINENT
+    echo "What is your timezone in that continent (often capital city)"
+	ls /usr/share/zoneinfo/$CONTINENT
+ read ZONE
  	arch-chroot /mnt ln -sf /usr/share/zoneinfo/$CONTINENT/$ZONE /etc/localtime
   	echo "DONE!"
    
